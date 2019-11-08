5df8125511f5c76ac7cf689dfc46dfaf0801de72
Panos <panos@ItIsNotAccurate.com>
2019-11-08 10:13:55 +0100

    Comment to specify the data sources

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 
+# dictionary through which we access the data files
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv'}
